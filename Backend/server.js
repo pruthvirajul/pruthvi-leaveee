212b0e53f96a06b46e833df3a9e7e17ed0af73c8
project-3 <your-github-pruthvirajul2001@gmail.com@example.com>
2025-07-08 12:55:10 +0530

    changes

--- Backend/server.js
+++ Backend/server.js
@@ -5,7 +5,7 @@ const multer = require('multer');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3600;
 
 // Middleware
 app.use(cors());
@@ -17,7 +17,7 @@ const pool = new Pool({
     host: 'postgres',
     database: 'leave_management',
     password: 'admin321',
-    port: 5432,
+    port: 5600,
 });
 
 // Configure Multer for file uploads
